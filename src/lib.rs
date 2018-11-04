168312b0f31fddee495e00595dc1cecd009518d8
Fuyang Liu <liufuyang@users.noreply.github.com>
2018-11-04 15:26:45 +0100

    model store wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 pub struct Category {
     pub name: String,
@@ -23,49 +23,75 @@ impl Model<ModelHashMapStore> {
 }
 
 pub trait ModelStore {
+    fn add_to_priors_count_of_class(&mut self, model_name: &str, c: &str, v: usize);
+    fn get_priors_count_of_class(&self, model_name: &str, c: &str) -> usize;
+    fn get_all_classes(&self, model_name: &str) -> Option<&HashSet<String>>;
+
     fn add_to_total_data_count(&mut self, model_name: &str, v: usize);
     fn get_total_data_count(&self, model_name: &str) -> usize;
 }
 
 // A in memory ModelStore implementation ModelHashMapStore
 pub struct ModelHashMapStore {
-    total_data_count: HashMap<String, usize>,
-    count_of_word_in_class: HashMap<String, usize>,
-    count_of_all_word_in_class: HashMap<String, usize>,
-    words_appeared: HashMap<String, usize>,
+    map: HashMap<String, usize>,
+    class_map: HashMap<String, HashSet<String>>, // model_name to list of class
+    word_map: HashMap<String, usize>,            // a very large hash map for look up word counts
 }
 
 impl ModelHashMapStore {
     pub fn new() -> ModelHashMapStore {
         ModelHashMapStore {
-            total_data_count: HashMap::new(),
-            count_of_word_in_class: HashMap::new(),
-            count_of_all_word_in_class: HashMap::new(),
-            words_appeared: HashMap::new(),
+            map: HashMap::new(),
+            class_map: HashMap::new(),
+            word_map: HashMap::new(),
         }
     }
 
     pub fn new_large() -> ModelHashMapStore {
         ModelHashMapStore {
-            total_data_count: HashMap::with_capacity(32),
-            count_of_word_in_class: HashMap::with_capacity(262_144), // 2^18
-            count_of_all_word_in_class: HashMap::with_capacity(256),
-            words_appeared: HashMap::with_capacity(32),
+            map: HashMap::with_capacity(1024),
+            class_map: HashMap::with_capacity(1024),
+            word_map: HashMap::with_capacity(262_144), // 2^18
         }
+    }
+
+    fn map_add(&mut self, model_name: &str, prefix: &str, v: usize) {
+        let key = format!("{}|%{}%", model_name, prefix);
+        let total_count = self.map.entry(key).or_insert(0);
+        *total_count += v;
+    }
+
+    fn map_get(&self, model_name: &str, prefix: &str) -> usize {
+        let key = format!("{}|%{}%", model_name, prefix);
+        *self.map.get(&key).unwrap_or_else(|| &0)
     }
 }
 
 impl ModelStore for ModelHashMapStore {
-    fn add_to_total_data_count(&mut self, model_name: &str, v: usize) {
-        let total_count = self
-            .total_data_count
+    fn add_to_priors_count_of_class(&mut self, model_name: &str, c: &str, v: usize) {
+        self.map_add(model_name, &format!("priors_count_of_class|%{}%", c), v);
+
+        let class_vec = self
+            .class_map
             .entry(model_name.to_string())
-            .or_insert(0);
-        *total_count += v;
+            .or_insert(HashSet::new());
+        class_vec.insert(c.to_string());
+    }
+
+    fn get_priors_count_of_class(&self, model_name: &str, c: &str) -> usize {
+        self.map_get(model_name, &format!("priors_count_of_class|%{}%", c))
+    }
+
+    fn get_all_classes(&self, model_name: &str) -> Option<&HashSet<String>> {
+        self.class_map.get(model_name)
+    }
+
+    fn add_to_total_data_count(&mut self, model_name: &str, v: usize) {
+        self.map_add(model_name, "total_data_count", v);
     }
 
     fn get_total_data_count(&self, model_name: &str) -> usize {
-        *self.total_data_count.get(model_name).unwrap_or_else(|| &0)
+        self.map_get(model_name, "total_data_count")
     }
 }
 
--- tests/lib.rs
+++ tests/lib.rs
@@ -15,6 +15,50 @@ mod tests {
     }
 
     #[test]
+    fn model_class_map_works() {
+        let model_store = ModelHashMapStore::new();
+        assert_eq!(
+            0,
+            model_store.get_priors_count_of_class("test_model", "class_1")
+        );
+        assert_eq!(
+            0,
+            model_store.get_priors_count_of_class("test_model", "class_2")
+        );
+        assert_eq!(None, model_store.get_all_classes("test_model"));
+
+        let mut model_store = model_store;
+
+        model_store.add_to_priors_count_of_class("test_model", "class_1", 1);
+        assert_eq!(
+            1,
+            model_store.get_priors_count_of_class("test_model", "class_1")
+        );
+        assert_eq!(1, model_store.get_all_classes("test_model").unwrap().len());
+
+        model_store.add_to_priors_count_of_class("test_model", "class_1", 10);
+        assert_eq!(
+            11,
+            model_store.get_priors_count_of_class("test_model", "class_1")
+        );
+        assert_eq!(1, model_store.get_all_classes("test_model").unwrap().len());
+
+        model_store.add_to_priors_count_of_class("test_model", "class_2", 10);
+        assert_eq!(
+            10,
+            model_store.get_priors_count_of_class("test_model", "class_2")
+        );
+        assert_eq!(2, model_store.get_all_classes("test_model").unwrap().len());
+
+        model_store.add_to_priors_count_of_class("test_model", "class_2", 10);
+        assert_eq!(
+            20,
+            model_store.get_priors_count_of_class("test_model", "class_2")
+        );
+        assert_eq!(2, model_store.get_all_classes("test_model").unwrap().len());
+    }
+
+    #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
     }
