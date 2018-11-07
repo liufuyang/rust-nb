18ed2a26b615df397455478ee24195a305cc4192
Fuyang Liu <fli@tradeshift.com>
2018-11-07 15:01:39 +0100

    Include gaussian model

--- tests/lib.rs
+++ tests/lib.rs
@@ -80,4 +80,74 @@ mod rust_nb {
         assert!((0.2 - *result.get("sad").unwrap()).abs() < 1e-10);
     }
 
+    #[test]
+    fn model_gaussian_works() {
+        let mut model = Model::new();
+
+        let input_train = vec![
+            (
+                "eur".to_owned(),
+                vec![Feature {
+                    feature_type: FeatureType::Gaussian,
+                    name: "age".to_owned(),
+                    value: "20".to_owned(),
+                }],
+            ),
+            (
+                "eur".to_owned(),
+                vec![Feature {
+                    feature_type: FeatureType::Gaussian,
+                    name: "age".to_owned(),
+                    value: "30".to_owned(),
+                }],
+            ),
+            (
+                "usd".to_owned(),
+                vec![Feature {
+                    feature_type: FeatureType::Gaussian,
+                    name: "age".to_owned(),
+                    value: "40".to_owned(),
+                }],
+            ),
+            (
+                "usd".to_owned(),
+                vec![Feature {
+                    feature_type: FeatureType::Gaussian,
+                    name: "age".to_owned(),
+                    value: "50".to_owned(),
+                }],
+            ),
+            (
+                "eur".to_owned(),
+                vec![Feature {
+                    feature_type: FeatureType::Gaussian,
+                    name: "age".to_owned(),
+                    value: "40".to_owned(),
+                }],
+            ),
+        ];
+        model.train("test_model", input_train);
+
+        let input_test = vec![Feature {
+            feature_type: FeatureType::Gaussian,
+            name: "age".to_owned(),
+            value: "23".to_owned(),
+        }];
+        let result = model.predict("test_model", &input_test).unwrap();
+
+        println!("{:?}", result);
+
+        let mut pEUR = 0.0312254; // https://www.wolframalpha.com/input/?i=normalpdf(23.0,+30.0,+10.0)
+        let mut pUSD = 0.000446108; // https://www.wolframalpha.com/input/?i=normalpdf(23.0,+45.0,+7.0710678118654755)
+
+        pUSD = pUSD * (2.0 / 5.0);
+        pEUR = pEUR * (3.0 / 5.0);
+
+        pUSD = pUSD / (pUSD + pEUR);
+        pEUR = 1.0 - pUSD;
+
+        assert!((pUSD - *result.get("usd").unwrap()).abs() < 1e-6);
+        assert!((pEUR - *result.get("eur").unwrap()).abs() < 1e-6);
+    }
+
 }
