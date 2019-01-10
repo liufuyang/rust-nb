3cd93df0fdbe52af85dd18c553411edf62a0e32a
Fuyang Liu <liufuyang@users.noreply.github.com>
2019-01-10 23:18:28 +0100

    Use memory efficient test code

--- examples/20newsgroup/main.rs
+++ examples/20newsgroup/main.rs
@@ -1,8 +1,11 @@
+extern crate rayon;
 extern crate rust_nb;
 
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
+
+use rayon::prelude::*;
 
 use rust_nb::{Feature, FeatureType, Model};
 
@@ -10,38 +13,38 @@ fn main() {
     let mut model = Model::new();
 
     let train_data = load_txt("examples/data/20newsgroup_train.txt");
-    let test_data = load_txt("examples/data/20newsgroup_test.txt");
-    let (test_labels, test_features): (Vec<String>, Vec<Vec<Feature>>) =
-        test_data.into_iter().map(|(s, v)| (s, v)).unzip();
+    let test_data = load_txt100("examples/data/20newsgroup_test.txt");
+
+    let test_data_len = test_data.len();
 
     println!(
         "Train size: {}, test size: {}",
         train_data.len(),
-        test_features.len()
+        test_data.len()
     );
 
     model.train("20newsgroup_model", &train_data);
     println!("Training finished");
 
-    let predicts = model.predict_batch("20newsgroup_model", &test_features);
-    println!("Testing finished");
-
-    let total_test_score: f64 = test_labels
-        .iter()
-        .zip(predicts.iter())
-        .map(|(test_label, predict)| {
+    let total_test_score: f64 = test_data
+        .into_par_iter()
+        .map(|(test_label, features)| {
+            let predict = model.predict("20newsgroup_model", &features);
             let (pred_label, _test_score) = predict
                 .iter()
                 .max_by(|(_ka, va), (_kb, vb)| va.partial_cmp(vb).unwrap())
                 .unwrap();
-            if test_label == pred_label {
+            if &test_label == pred_label {
                 1.0
             } else {
                 0.0
             }
         })
         .sum();
-    let score = total_test_score / test_labels.len() as f64;
+
+    println!("Testing finished");
+
+    let score = total_test_score / test_data_len as f64;
 
     println!("test score: {}", score);
     assert!((0.5771375464684015 - score).abs() < 1e-10);
