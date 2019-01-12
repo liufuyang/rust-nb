9214ce526bd22648deb9379507c416c19b132498
Fuyang Liu <fli@tradeshift.com>
2019-01-12 22:45:13 +0100

    Reduce memory usage on prediction

--- examples/ts-pc/main.rs
+++ examples/ts-pc/main.rs
@@ -1,47 +1,48 @@
+extern crate rayon;
 extern crate rust_nb;
 
+use rayon::prelude::*;
+use rust_nb::{Feature, FeatureType, Model};
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
-
-use rust_nb::{Feature, FeatureType, Model};
 
 fn main() {
     let mut model = Model::new().with_pseudo_count(0.01);
 
     let train_data = load_txt(".hidden/train.csv");
     let test_data = load_txt(".hidden/test.csv");
-    let (test_labels, test_features): (Vec<String>, Vec<Vec<Feature>>) =
-        test_data.into_iter().map(|(s, v)| (s, v)).unzip();
+
+    let test_data_len = test_data.len();
 
     println!(
         "Train size: {}, test size: {}",
         train_data.len(),
-        test_features.len()
+        test_data.len()
     );
 
     model.train("pc", &train_data);
     println!("Training finished");
 
-    let predicts = model.predict_batch("pc", &test_features);
-    println!("Testing finished");
-
-    let total_test_score: f64 = test_labels
-        .iter()
-        .zip(predicts.iter())
-        .map(|(test_label, predict)| {
+    let total_test_score: f64 = test_data
+        .into_par_iter()
+        .map(|(test_label, features)| {
+            let predict = model.predict("pc", &features);
             let (pred_label, _test_score) = predict
                 .iter()
                 .max_by(|(_ka, va), (_kb, vb)| va.partial_cmp(vb).unwrap())
                 .unwrap();
-            if test_label == pred_label {
+            if &test_label == pred_label {
                 1.0
             } else {
                 0.0
             }
         })
         .sum();
-    let score = total_test_score / test_labels.len() as f64;
+
+    println!("Testing finished");
+
+    let score = total_test_score / test_data_len as f64;
 
     println!("test score: {}", score);
 }
@@ -55,12 +56,9 @@ fn load_txt(file_name: &str) -> Vec<(String, Vec<Feature>)> {
     for line in f.lines() {
         let line = line.unwrap();
         let line_v: Vec<&str> = line.rsplitn(2, ",").collect();
-        
-        let unspsc = line_v.get(0).unwrap_or(&"UNKNOWN").to_string();
-        let product_name  = line_v.get(1).unwrap_or(&"").to_string();
 
-        // print!("unspsc: {}, ", unspsc);
-        // println!("product name: {}", product_name);
+        let unspsc = line_v.get(0).unwrap_or(&"UNKNOWN").to_string();
+        let product_name = line_v.get(1).unwrap_or(&"").to_string();
 
         v.push((
             unspsc,
